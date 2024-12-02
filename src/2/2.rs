ed4639ff892a6673384553a5c288344153791288
pedrosoutinho <johnetterna@protonmail.com>
2024-12-02 02:50:24 -0300

    copy

--- src/2/2.rs
+++ src/2/2.rs
@@ -1,37 +1,40 @@
 use std::io;
 
 fn main() {
-    let mut ans : i32 = 0;
+    let mut ans: i32 = 0;
 
     let mut line = String::new();
     while let Ok(n) = io::stdin().read_line(&mut line) {
         if n == 0 {
             break;
         }
-        let mut iter = line.split_whitespace();
+        let iter = line.split_whitespace();
 
         let mut v = Vec::<i32>::new();
-        while let Some(x) = iter.next() {
+        for x in iter {
             v.push(x.parse().unwrap());
         }
 
         for j in 0..v.len() {
             let mut v2 = Vec::<i32>::new();
 
-            for i in 0..v.len() {
-                if i != j {
-                    v2.push(v[i]);
+            for (i, x) in v.iter().enumerate() {
+                if i == j {
+                    continue;
                 }
+
+                v2.push(*x);
             }
 
-            let mut f1 : bool = true;
-            let mut f2 : bool = true;
+            let mut f1: bool = true;
+            let mut f2: bool = true;
 
-            let mut prev : i32 = v2[0];
-            for i in 1..v2.len() {
-                f1 &=  (prev + 1 <= v2[i]) & (prev + 3 >= v2[i]);
-                f2 &= (prev >= v2[i] + 1) & (prev <= v2[i] + 3);
-                prev = v2[i];
+            let mut prev: i32 = v2[0];
+            for it in v2.iter().skip(1) {
+                let x = *it;
+                f1 &= (prev < x) & (prev + 3 >= x);
+                f2 &= (prev > x) & (prev <= x + 3);
+                prev = x;
             }
 
             if f1 || f2 {
