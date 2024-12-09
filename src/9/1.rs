6b6dc02dec2a712081ee8278965bd04b9662c320
pedrosoutinho <johnetterna@protonmail.com>
2024-12-09 03:11:36 -0300

    dia 9

--- src/9/1.rs
+++ src/9/1.rs
@@ -5,45 +5,38 @@ fn main() {
     io::stdin().read_line(&mut buf).unwrap();
     let mut line = vec![];
     let mut id = 0;
-    let mut space = vec![];
-    let mut block = vec![];
     for i in 0..buf.len() - 1 {
         let x: usize = buf.trim().chars().nth(i).unwrap().to_digit(10).unwrap() as usize;
         if i % 2 == 0 {
-            block.push((line.len(), x));
             for _ in 0..x {
                 line.push(id);
             }
             id += 1;
         } else {
-            space.push((line.len(), x));
-            line.resize(line.len() + x, 0);
+            for _ in 0..x {
+                line.push(-1);
+            }
         }
     }
 
-    for a in (0..block.len()).rev() {
-        let (i, x) = block[a];
-        let id = line[i];
+    for i in 0..line.len() {
+        if i >= line.len() {
+            break;
+        }
 
-        for val in space.iter_mut() {
-            let (j, y) = *val;
-            if i <= j {
+        if line[i] != -1 {
+            continue;
+        }
+
+        loop {
+            let x = line.pop().unwrap();
+            if x == -1 {
                 continue;
             }
-
-            if x > y {
-                continue;
+            if i < line.len() {
+                line[i] = x;
             }
 
-            for val in line.iter_mut().skip(i).take(x) {
-                *val = 0;
-            }
-
-            for val in line.iter_mut().skip(j).take(x) {
-                *val = id;
-            }
-
-            *val = (j + x, y - x);
             break;
         }
     }
