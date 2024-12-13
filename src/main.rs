5da3dcfae45340b897fda20f7242ec5126d87972
pedrosoutinho <johnetterna@protonmail.com>
2024-12-13 03:31:24 -0300

    dia 11

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,46 @@
+use std::collections::HashMap;
 use std::io;
 
 fn main() {
     let mut buf: String = String::new();
-    let mut ans: i64 = 0;
+    let mut cnt: HashMap<i64, i64> = HashMap::new();
     while let Ok(n) = io::stdin().read_line(&mut buf) {
         if n == 0 {
             break;
         }
 
-        let mut dx1 = 0;
-        let mut tmp = String::new();
-        for c in buf.trim().chars() {
-            if c.is_ascii_digit() {
-                tmp.push(c);
-            } else if c == ',' {
-                dx1 = tmp.parse::<i64>().unwrap();
-                tmp.clear();
-            }
+        for x in buf.split_whitespace().map(|x| x.parse::<i64>().unwrap()) {
+            *cnt.entry(x).or_insert(0) += 1;
         }
-        let dy1 = tmp.parse::<i64>().unwrap();
-        tmp.clear();
-
-        let mut dx2 = 0;
-        buf.clear();
-        io::stdin().read_line(&mut buf).unwrap();
-        for c in buf.trim().chars() {
-            if c.is_ascii_digit() {
-                tmp.push(c);
-            } else if c == ',' {
-                dx2 = tmp.parse::<i64>().unwrap();
-                tmp.clear();
-            }
-        }
-        let dy2 = tmp.parse::<i64>().unwrap();
 
         buf.clear();
-        io::stdin().read_line(&mut buf).unwrap();
-        let mut px = 0;
-        let mut tmp = String::new();
-        for c in buf.trim().chars() {
-            if c.is_ascii_digit() {
-                tmp.push(c);
-            } else if c == ',' {
-                px = tmp.parse::<i64>().unwrap();
-                tmp.clear();
-            }
-        }
-        let mut py = tmp.parse::<i64>().unwrap();
-        buf.clear();
-        io::stdin().read_line(&mut buf).unwrap();
-        buf.clear();
-
-        let det = dx1 * dy2 - dx2 * dy1;
-        if det == 0 {
-            continue;
-        }
-
-        // pt2
-        let shift = 10000000000000;
-        px += shift;
-        py += shift;
-
-        let mut i = dy2 * px - py * dx2;
-        let mut j = -dy1 * px + py * dx1;
-        if (i % det != 0) || (j % det != 0) {
-            continue;
-        }
-
-        i /= det;
-        j /= det;
-        if i < 0 || j < 0 {
-            continue;
-        }
-
-        ans += 3 * i + j;
     }
 
+    let pot10: Vec<i64> = (0..19).map(|x| 10i64.pow(x)).collect();
+
+    // pt1: 25, pt2: 75
+    for _ in 0..75 {
+        let mut ncnt: HashMap<i64, i64> = HashMap::new();
+        for (k, v) in cnt.iter() {
+            let l = k.to_string().len();
+            if *k == 0 {
+                *ncnt.entry(1).or_insert(0) += *v;
+            } else if l % 2 == 0 {
+                let x = *k / pot10[l / 2];
+                let y = *k % pot10[l / 2];
+                *ncnt.entry(x).or_insert(0) += *v;
+                *ncnt.entry(y).or_insert(0) += *v;
+            } else {
+                *ncnt.entry((*k) * 2024).or_insert(0) += *v;
+            }
+        }
+
+        cnt = ncnt;
+    }
+
+    let mut ans: i64 = 0;
+    for (_, v) in cnt.iter() {
+        ans += v;
+    }
     println!("{}", ans);
 }
